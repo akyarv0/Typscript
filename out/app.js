74d55415f8b6828b8cad057e859b01efa56b6031
akyarv0 <akyarv0@gmail.com>
2024-05-25 02:12:12 +0300

    first commit

---
+++ out/app.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("Hello World");
+let x = 8;
+let y = "test";
+const z = (a, b, c) => a + b;
---
+++ src/app.ts
@@ -0,0 +1,8 @@
+console.log("Hello World");
+let x = 8
+//console.log(x);
+
+// comment silindi
+let y = "test"
+
+const z = (a,b, c) => a + b
