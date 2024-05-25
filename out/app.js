09ddcfe96668119fd71c975ae7d7b48d26feb76c
akyarv0 <akyarv0@gmail.com>
2024-05-25 04:01:19 +0300

    void

--- out/app.js
+++ out/app.js
@@ -1,2 +1,17 @@
-"use strict";
+
+console.log("Hello World");
 let a = 8;
+let list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let list2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+list.push(11);
+console.log(list2);
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+let c = Color.Red;
+console.log(c);
+console.log(c);
+console.log("hello");
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-// console.log("Hello World");
+console.log("Hello World");
 // let x = 8
 // //console.log(x);
 
@@ -8,3 +8,20 @@
 // const z = (a,b, c) => a + b
 
 let a : number = 8
+//!Arrays
+
+let list: number[] = [1,2,3,4,5,6,7,8,9,10]
+let list2 : Array<number> = [1,2,3,4,5,6,7,8,9,10]
+list.push(11)
+console.log(list2);
+
+ enum Color {
+    Red,
+    Green,
+    Blue
+} 
+
+let c: Color = Color.Red
+console.log(c);
+
+console.log("hello");
